f52d4495a08435cabbf6a36ae540bb7a529a619e
miyasajid19 <miyasajid19@gmail.com>
2024-09-07 12:47:59 +0530

    add solution for Question no.2 iv

---
+++ Fixed Point Iteration Method/Question_2_iv.m
@@ -0,0 +1,14 @@
+g=@(x) (10/(4+x))^(1/2);
+e=10^(-3);
+n=1000;
+x0=1.5;
+i=0;
+while(i<=n)
+    x1=g(x0);
+    if ((abs(x1-x0)<e) || (abs(x1-x0)/abs(x1))<e)
+        break;
+    end
+    x0=x1;
+    i=i+1;
+end
+fprintf('the root is found to be %f in %d iterations',x1,i);
