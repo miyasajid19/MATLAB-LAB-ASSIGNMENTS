7ddbb705b791ee078f3236e374f6def02721e130
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-19 09:52:39 +0530

    add solution for fifth question part 2

--- Newton Rapson's method/Question_5_ii.m
+++ Newton Rapson's method/Question_5_ii.m
@@ -4,24 +4,27 @@ fx = @(x,y) 2*x;
 fy = @(x,y) 2*y;
 gx = @(x,y) 4*x;
 gy = @(x,y) 6*y;
+
 X = [1; 1];
 tol = 0.0001;
-error = 1;
+error = 1; 
 clc;
 format long;
-i=0;
+i = 0; 
+
 while error > tol
     jacobian = [fx(X(1),X(2)), fy(X(1),X(2)); gx(X(1),X(2)), gy(X(1),X(2))];
     F = [f(X(1), X(2)); g(X(1), X(2))];
     X1 = X - inv(jacobian) * F;
-    i
-    X1
+
     error = max(abs(X1 - X)); 
-    X = X1;
-    i=i+1;
+    X = X1; 
+    i = i + 1; 
 end
-i
-fprintf("the solution is :: ");
-disp(X');
-f(X(1), X(2)) % Should be close to 0
-g(X(1), X(2)) % Should be close to 0
+
+fprintf("--------------------------------------------------\n");
+fprintf("Converged after %d iterations.\n", i);
+fprintf("The solution is :: X1 = %.6f, X2 = %.6f\n", X(1), X(2));
+
+fprintf("f(X) = %.10f (should be close to 0)\n", f(X(1), X(2)));
+fprintf("g(X) = %.10f (should be close to 0)\n", g(X(1), X(2)));
