f704bc5d133f03b0f3a10b26b4e77472e9d2ced7
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-19 21:56:20 +0530

    add solution for the second question

---
+++ Fitting of Graphs/Question_2.m
@@ -0,0 +1,42 @@
+x = [-2,-1,0,1,2];
+y = [15,1,1,3,19];
+ysum = 0;
+xsum = 0;
+xy = 0;
+x2 = 0;
+for i = 1:length(x)
+    ysum = ysum + y(i);
+    xsum = xsum + x(i);
+    xy = xy + x(i)*y(i);
+    x2 = x2 + x(i)^2;
+end
+length(x)
+A = [length(x), xsum, ysum; xsum, x2, xy];
+
+n = 2;
+for i = 1:n
+    if A(i,i) == 0
+        for j = i+1:n
+            if A(j,i) ~= 0
+                A([i, j], :) = A([j, i], :);
+                disp(['Swapped row ', num2str(i), ' with row ', num2str(j)]);
+                break;
+            end
+        end
+    end
+    if A(i,i) ~= 0
+        for j = i+1:n
+            factor = A(j,i) / A(i,i);
+            A(j,:) = A(j,:) - factor * A(i,:);
+        end
+    end
+end
+
+disp('Upper triangular matrix A:');
+disp(A);
+
+x_sol = zeros(n, 1);
+for i = n:-1:1
+    x_sol(i) = (A(i,end) - A(i, 1:end-1) * x_sol) / A(i,i);
+end
+disp(['The linear equation is: y = ', num2str(x_sol(2)),' x + (', num2str(x_sol(1)),')']);
