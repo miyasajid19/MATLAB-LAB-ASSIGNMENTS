c8fccbc0b99d39428b33fea4cb047c18cb23ce0e
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-19 22:09:48 +0530

    add solution for the first part

---
+++ Power Method/Question_7_a.m
@@ -0,0 +1,35 @@
+A = [
+    4, 1, 0;
+    1, 20, 1;
+    0, 1, 4;
+]; % Defining matrix A
+
+x = [1; 1; 1]; % Initial guess for eigenvector
+k = 1000; % Initial guess for eigenvalue
+
+for i = 1:10000
+    y = A * x; % Calculate y = A * x
+    k1 = max(abs(y)); % Find the largest magnitude of y and assign to k1
+    x1 = (1 / k1) * y; % Normalize y to obtain the next approximation of the eigenvector
+    
+    % Check for convergence in eigenvalue approximation
+    if (abs(k - k1) < 10^-3)
+        x = x1; % Update eigenvector
+        k = k1; % Update eigenvalue
+        break;
+    end
+    
+    % Update for next iteration
+    x = x1;
+    k = k1;
+end
+
+% Display the final results
+disp('Estimated Eigenvector (normalized):');
+disp(x1);
+
+disp('Largest Eigenvalue:');
+disp(k);
+
+disp('Total Iterations:');
+disp(i);
