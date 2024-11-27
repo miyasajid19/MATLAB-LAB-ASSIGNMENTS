de97d17f3a2700d2b13912d3e7a9ae278de4f3c6
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-27 21:42:10 +0530

    composite simpson's rule

--- REVISION/Simpsons_Rule.m
+++ REVISION/Simpsons_Rule.m
@@ -3,12 +3,20 @@ n=20;
 f=@(x) cos(x)^2;
 a=-0.25;
 b=0.25;
-h=(b-a)/n;
-sum=0;
-for i=1:n-1
-    x=a+h*i;
-    sum=sum+2*f(x);
+if(mod(n,2)==0)
+    sum=0;
+    h=(b-a)/n;
+    for i=1:n-1
+        x=a+h*i;
+        if(mod(i,2)==0)
+            sum=sum+2*f(x);
+        else
+            sum=sum+4*f(x);
+        end
+    end
+    sum=sum+f(a)+f(b);
+    soln=(h/3)*sum;
+    fprintf("the solution is found to be %f",soln);
+else
+    error("intervals should be even")
 end
-sum=sum+f(a)+f(b);
-soln=(h/2)*sum;
-fprintf("The solution is %f",soln)
