c5d0b634f3aa692916df1836e489ac5b021d5937
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-28 01:54:43 +0530

    Gauss elimination method

--- REVISION/linearRegression.m
+++ REVISION/linearRegression.m
@@ -1,46 +1,59 @@
-x=[-2,-1,0,1,2];
-y=[12,1,1,3,19];
-X=0;
-Y=0
-XX=0;
-XY=0;
-for i=1:length(x)
-    X=X+x(i);
-    Y=Y+y(i);
-    XX=XX+x(i)*x(i);
-    XY=XY+x(i)*y(i);
+x = [-2,-1,0,1,2];
+y = [12,1,1,3,19];
+
+X = 0;
+Y = 0;
+XX = 0;
+XY = 0;
+
+% Summing the necessary values
+for i = 1:length(x)
+    X = X + x(i);
+    Y = Y + y(i);
+    XX = XX + x(i) * x(i);
+    XY = XY + x(i) * y(i);
 end
-A=[length(x),X,Y;X,XX,XY];
-for i=1:2
-    if(A(i,i)==0)
-        
-        for j=i+1:2
-            if(A(j,i)~=0)
-                A([i,j],:)=A([j,i],:);
-            end        
+
+% Construct the augmented matrix for the system of equations
+A = [length(x), X, Y; X, XX, XY];
+
+% Forward elimination
+for i = 1:2
+    if (A(i,i) == 0)
+        for j = i+1:2
+            if (A(j,i) ~= 0)
+                A([i,j], :) = A([j,i], :); % Swap rows
+            end
         end
-    end     
-    if(A(i,i)==0)
-        error("can't solve");
     end
-    %for ward elimination
-    for j=i+1:2
-        m=A(j,i)/A(i,i);
-        A(j,:)=A(j,:)-m*A(i,:);
+    if (A(i,i) == 0)
+        error("Can't solve");
+    end
+    
+    % Perform Gaussian elimination
+    for j = i+1:2
+        m = A(j,i) / A(i,i);
+        A(j,:) = A(j,:) - m * A(i,:);
     end
 end
-if(A(2,2)==0)
+
+% Check for infinite solutions
+if (A(2,2) == 0)
     error("Infinite solution");
 end
 
-%backward subsitution
-x=zeros(2,1);
-x(2)=A(2,end)/A(2,2);
-for j=n-1:-1:1
-    sum=0;
-    for k=j+1:2
-        sum=sum+A(j,k)*x(j);
+% Backward substitution
+x_vals = zeros(2,1);  % Initialize solution vector
+x_vals(2) = A(2,end) / A(2,2);  % Solve for x(2)
+
+% Perform backward substitution for the first unknown
+for j = 1:1
+    sum = 0;
+    for k = j+1:2
+        sum = sum + A(j,k) * x_vals(k); % Correct summation term
     end
-    x(j)=(A(j,end)-sum)/A(j,j);
+    x_vals(j) = (A(j,end) - sum) / A(j,j);
 end
-x
+
+% Display the solution
+x_vals
