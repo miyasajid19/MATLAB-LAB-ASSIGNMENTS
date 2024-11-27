7f39e93e926f3f440a6be2cd4f481684a8b17790
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-28 01:12:01 +0530

    power method

--- REVISION/powerMethod_1.m
+++ REVISION/powerMethod_1.m
@@ -1,24 +1,26 @@
 clc;
-%coefficient matrix
-A=[
-    4,1,-1,1;
-    1,4,-1,-1;
-    -1,-1,5,1;
-    1,-1,1,3;
+A = [
+    4, 1, -1, 1;
+    1, 4, -1, -1;
+    -1, -1, 5, 1;
+    1, -1, 1, 3;
 ];
-k=1000;
-x=[1,1,1,1]';
-for i=1:100000
-    y=A*x;
-    k1=max(abs(y));
-    x1=1/k1*y;
-    if(abs(k1-k)<0.00001)
-        k=k1;
+
+k = 1000;
+x = [1; 1; 1; 1];
+
+for i = 1:100000
+    y = A * x;
+    k1 = max(abs(y));
+    x1 = y / k1;
+    
+    if abs(k1 - k) < 0.00001
+        k = k1;
         break;
     end
-    x=x1;
-    k=k1;
+    
+    x = x1;
+    k = k1;
 end
 
-k
-g
+fprintf("The largest eigenvalue is %f\n", k);
