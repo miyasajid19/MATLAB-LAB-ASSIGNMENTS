e796ac25852fb747807c9e63cd844b919216e9f8
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-28 08:14:01 +0530

    qr method

--- REVISION/QR_Method.m
+++ REVISION/QR_Method.m
@@ -1,40 +1,43 @@
-A=[
-    1,1,0;
-    1,0,1;
-    0,1,1;
-];
-clc;
-function [Q]=gram_schmidt(A)
-    [m,n]=size(A);
-    Q=zeros(m,n);
-    for i=1:n
-        v=A(:,i);
-        for j=1:i-1
-            projection=Q(:,j)'*v;
-            v=v-projection*Q(:,j);
+function main
+    A = [
+        1, 1, 0;
+        1, 0, 1;
+        0, 1, 1;
+    ];
+    Ak = qr_iteration(A);
+    disp('Diagonal elements of Ak (eigenvalues):');
+    disp(diag(Ak));
+    disp('Final Ak matrix:');
+    disp(Ak);
+end
+
+function [Q] = gram_schmidt(A)
+    [m, n] = size(A);
+    Q = zeros(m, n);
+    for i = 1:n
+        v = A(:, i);
+        for j = 1:i-1
+            projection = (Q(:, j)' * v) / (Q(:, j)' * Q(:, j));
+            v = v - projection * Q(:, j);
         end
-        Q(:,i)=v/norm(v);
+        Q(:, i) = v / norm(v);
     end
 end
 
-function [Ak]=qr(A)
-    [m,n]=size(A);
-    if(m~=n)
-        error("must be square matrix");
+function [Ak] = qr_iteration(A)
+    [m, n] = size(A);
+    if m ~= n
+        error("Matrix must be square.");
     end
-    Ak=A;
-    for i=1:1000
-        Q=gram_schmidt(A);
-        R=inv(Q)*Ak;
-        Ak_next=R*Q;
-        if(norm(Ak_next-Ak)<0.000001)
-            Ak=Ak_next;
+    Ak = A;
+    for i = 1:1000
+        Q = gram_schmidt(Ak);
+        R = Q' * Ak;
+        Ak_next = R * Q;
+        if norm(Ak_next - Ak) < 1e-6
+            Ak = Ak_next;
             break;
         end
-        Ak=Ak_next;
+        Ak = Ak_next;
     end
 end
-
-Ak=qr(A);
-diag(Ak)
-Ak
