7495c5572e5df96169b3cd0e6c1b5329cab17604
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-12-02 09:44:22 +0530

    Fixed Point Iteration Method

---
+++ REVISION/fixedPoint.m
@@ -0,0 +1,12 @@
+f=@(x) 2*sin(pi*x)+x;
+g=@(x) x-(2*sin(pi*x)+1)/(s*pi*cos(pi* x)+1);
+x=1;
+for i=1:1000000
+    x1=g(x);
+    if(abs(x1-x)<0.00001)
+        x=x1;break;
+    end
+    x=x1;
+end
+x
+i
